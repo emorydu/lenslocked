795c0e8e494c1c4fe2dd28525562066de645bf9c
Emory.Du <Emory163@163.com>
2023-09-12 20:21:27 +0800

    feat: contact & path

--- main.go
+++ main.go
@@ -10,8 +10,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,
+		"<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"orangeduxiaocheng@gmail.com\">orangeduxiaocheng@gmail.com</a>.")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/path", pathHandler)
 	fmt.Println("Starting the server on: 3000...")
 	http.ListenAndServe(":3000", nil)
 }
