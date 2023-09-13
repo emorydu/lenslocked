bf2380ca3f8ce22bd8d1293146c1f52ab4d0c193
Emory.Du <Emory163@163.com>
2023-09-13 11:05:10 +0800

    feat: add faq page

---
+++ Exercises.md
@@ -0,0 +1,18 @@
+# Exercises
+
+1. Add an FAQ page.
+
+We want visit <127.0.0.1:3000/faq> and have it render an FAQ page with
+questions and answer like:
+
+```
+Q: Is there a free version?
+A: Yes! We offer a free trial for 30 days on any paid plans.
+
+Q: What are your support hours?
+A: We have support staff answering emails 24/7, though response times may be a
+bit slower on weekends.
+
+Q: How do I contact support?
+A: Email us - support@orangeduxiaocheng@gmail.com
+```
--- main.go
+++ main.go
@@ -12,7 +12,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w,
-		"<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"orangeduxiaocheng@gmail.com\">orangeduxiaocheng@gmail.com</a>.")
+		"<h1>Contact Page</h1><p>To get in touch, email me at <a href=\""+
+			"rangeduxiaocheng@gmail.com\">orangeduxiaocheng@gmail.com</a>.")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+	<li><b>Is there a free version? Yes!</b> We offer a free trial for 30 days on any
+  paid plans.</li>
+	<li><b>What are your support hours?</b> We have support staff answering emails 24/7, though response times may be a
+	bit slower on weekends.</li>
+	<li><b>How do I contact support?</b> Email us - <a href="mailto:support@lenslocked.com">support@orangeduxiaocheng@gmail.com</a></li>
+</ul>
+`)
 }
 
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +60,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
