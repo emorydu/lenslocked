591e5e0eb1d2b87daa4bfb7878f5286b51b4a911
Emory.Du <Emory163@163.com>
2023-09-13 14:59:42 +0800

    feat: using html/templates xss

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -5,24 +5,33 @@ import (
 	"os"
 )
 
+type User struct {
+	Name string
+	Bio  string
+}
+
 func main() {
 	t, err := template.ParseFiles("hello.gohtml")
 	if err != nil {
 		panic(err)
 	}
 
-	u := struct {
-		Name string
-		Age  uint8
-		Meta struct {
-			Visits int
-		}
-	}{
+	// u := struct {
+	// 	Name string
+	// 	Age  uint8
+	// 	Meta struct {
+	// 		Visits int
+	// 	}
+	// }{
+	// 	Name: "Emory.Du",
+	// 	Age:  24,
+	// 	Meta: struct{ Visits int }{
+	// 		Visits: 4,
+	// 	},
+	// }
+	u := User{
 		Name: "Emory.Du",
-		Age:  24,
-		Meta: struct{ Visits int }{
-			Visits: 4,
-		},
+		Bio:  `<script>alert("Haha, you have been h4x0r3d!"); </script>`,
 	}
 
 	err = t.Execute(os.Stdout, u)
--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// bio := `<script>alert("Haha, you have been h4x0r3d!"); </script>`
+	bio := `&lt;script&gt;alert(&quot;Hi!&quot;);&lt;/script&gt;`
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1><p>Bio:"+bio+"</p>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
