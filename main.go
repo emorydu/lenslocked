c2d0cabcec26386426c9af11895680e5ee9a7244
Emory.Du <Emory163@163.com>
2023-09-13 10:45:53 +0800

    mod: modify source code & learn http source code

--- main.go
+++ main.go
@@ -15,40 +15,46 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 		"<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"orangeduxiaocheng@gmail.com\">orangeduxiaocheng@gmail.com</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintln(w, r.URL.Path)
-	// fmt.Fprintln(w, r.URL.RawPath)
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		// TODO: handle the page not found error
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		// w.WriteHeader(http.StatusNotFound)
-		// fmt.Fprint(w, "404 Not Found")
-	}
-	// if r.URL.Path == "/" {
-	// 	homeHandler(w, r)
-	//  return
-	// } else if r.URL.Path == "/contact" {
-	// 	contactHandler(w, r)
-	//  return
-	// }
-}
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprintln(w, r.URL.Path)
+// 	// fmt.Fprintln(w, r.URL.RawPath)
 // 	switch r.URL.Path {
 // 	case "/":
 // 		homeHandler(w, r)
 // 	case "/contact":
 // 		contactHandler(w, r)
 // 	default:
+// 		// TODO: handle the page not found error
 // 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+// 		// w.WriteHeader(http.StatusNotFound)
+// 		// fmt.Fprint(w, "404 Not Found")
 // 	}
+// 	// if r.URL.Path == "/" {
+// 	// 	homeHandler(w, r)
+// 	//  return
+// 	// } else if r.URL.Path == "/contact" {
+// 	// 	contactHandler(w, r)
+// 	//  return
+// 	// }
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
+// type Server struct{}
+
+// func (s *Server) HomeHandler(w http.ResponseWriter, r *http.Request) {
+
 // }
 
 func main() {
@@ -59,11 +65,18 @@ func main() {
 
 	// http.Handle("/", http.Handler)
 
-	var router http.HandlerFunc = pathHandler
+	// var router http.HandlerFunc = pathHandler
 	// http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/contact", contactHandler)
 	// http.HandleFunc("/path", pathHandler)
-	// mux := Router{}
+	router := Router{}
+
+	// var s Server
+	// http.HandleFunc("/home", s.HomeHandler)
+
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on: 3000...")
 	http.ListenAndServe(":3000", router)
+	// http.ListenAndServe(":3000", nil)
 }
