008d16593a89b3930c44bcf0a3d0de06cbf01383
Emory.Du <Emory163@163.com>
2023-09-14 17:58:57 +0800

    feat: Added signup & Styled the signup Form

--- main.go
+++ main.go
@@ -20,6 +20,9 @@ func main() {
 	r.Get("/faq", controllers.FAQ(views.Must(
 		views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
+	r.Get("/signup", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
