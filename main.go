5a4c1baa19672173ea6e48b785dd82ae316cd234
Emory.Du <Emory163@163.com>
2023-09-14 14:57:17 +0800

    feat: Added must-function

--- main.go
+++ main.go
@@ -12,27 +12,17 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	t, err := views.Parse("home")
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/", controllers.StaticHandler(t))
-
-	t, err = views.Parse("contact")
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/contact", controllers.StaticHandler(t))
-
-	t, err = views.Parse("faq")
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/faq", controllers.StaticHandler(t))
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.Parse("home"))))
+	r.Get("/contact", controllers.StaticHandler(
+		views.Must(views.Parse("contact"))))
+	r.Get("/faq", controllers.StaticHandler(
+		views.Must(views.Parse("faq"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
+
 	fmt.Println("Starting the server on: 3000...")
 	http.ListenAndServe(":3000", r)
 }
--- views/template.go
+++ views/template.go
@@ -8,6 +8,13 @@ import (
 	"path/filepath"
 )
 
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
 func Parse(tmpl string) (Template, error) {
 	tmplPath := filepath.Join("templates", tmpl+".gohtml")
 	t, err := template.ParseFiles(tmplPath)
