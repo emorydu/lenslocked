56998c76dfc98577dfb46cdb9cdeefdc98c56880
Emory.Du <Emory163@163.com>
2023-09-14 16:24:43 +0800

    feat: Exploring two different layout setups

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "home.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "contact.gohtml"))))
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
