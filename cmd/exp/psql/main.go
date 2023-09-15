f390c9f61dd33bafb3acbe55b638626b57cea90b
Emory.Du <Emory163@163.com>
2023-09-15 11:30:59 +0800

    exp: Added a postgres config for providing postgres connection variables

---
+++ cmd/exp/psql/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (c PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Password, c.Database, c.SSLMode)
+}
+
+func main() {
+
+	cfg := PostgresConfig{
+		Host:     "127.0.0.1",
+		Port:     "5432",
+		User:     "EmoryRoland",
+		Password: "EmoryRoland",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected!")
+}
