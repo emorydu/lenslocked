45768ca022629d5e2f780bd24789a2f0d51bf1d2
Emory.Du <Emory163@163.com>
2023-09-15 14:07:38 +0800

    exp: Added execute-sql-with-go

--- cmd/exp/psql/main.go
+++ cmd/exp/psql/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
@@ -35,8 +37,114 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+
 	if err := db.Ping(); err != nil {
 		panic(err)
 	}
 	fmt.Println("Connected!")
+
+	// Create a table...
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS orders (
+			id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL,
+			amount INT,
+			description TEXT
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables created.")
+
+	// Insert some data...
+	// name := "Emory.Du"
+	// email := "example@gmail.com"
+	// _, err = db.Exec(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2);`, name, email)
+
+	// row := db.QueryRow(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2) RETURNING id;`, name, email)
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("User Created. id = ", id)
+
+	// Query Single Row
+	id := 1
+	row := db.QueryRow(`
+		SELECT name, email
+		FROM users
+		WHERE id = $1`, id)
+
+	var user struct {
+		name  string
+		email string
+	}
+
+	err = row.Scan(&user.name, &user.email)
+	if errors.Is(err, sql.ErrNoRows) {
+		fmt.Println("Error, no rows!")
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Query User:", user)
+
+	userID := 1
+	// for i := 0; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err := db.Exec(`
+	// 		INSERT INTO orders(user_id, amount, description)
+	// 		VALUES ($1, $2, $3)`, userID, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	// fmt.Println("Created fake orders")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+	var orders []Order
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		FROM orders
+		WHERE user_id=$1
+	`, userID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Orders:", orders)
 }
