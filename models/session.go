89bdd35a29da5849800850ed28d74fdbc4056c1e
Emory.Du <Emory163@163.com>
2023-09-20 16:22:32 +0800

    feat: Added query user via session

--- models/session.go
+++ models/session.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"github.com/emorydu/lenslocked/rand"
 )
@@ -13,7 +14,7 @@ type Session struct {
 	UserID int
 	// Token is only set when creating a new session. When look up a session
 	// this will be left empty, as we only store the hash of a session token
-	// in our database and we cannot reverse it into a raw token.
+	// in our database, and we cannot reverse it into a raw token.
 	Token     string
 	TokenHash string
 }
@@ -49,13 +50,53 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: ss.hash(token),
 	}
-	// TODO: Store the session in our DB
+	// Store the session in our DB
+	// 1. Try to update the user's session
+	// 2. If err, create a new session
+	row := ss.DB.QueryRow(`
+		UPDATE sessions
+		SET token_hash = $2
+		WHERE user_id = $1 RETURNING id;`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if errors.Is(err, sql.ErrNoRows) {
+		row = ss.DB.QueryRow(`
+		INSERT INTO sessions (user_id, token_hash) 
+		VALUES ($1, $2) 
+		RETURNING id;`, session.UserID, session.TokenHash)
+		err = row.Scan(&session.ID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
 	return &session, nil
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	// TODO: Implement SessionService.User
-	return nil, nil
+	// 1. Hash the session tokens
+	hasher := ss.hash(token)
+
+	// 2. Query for the session with that hash
+	var user User
+	row := ss.DB.QueryRow(`
+		SELECT user_id FROM sessions 
+		WHERE token_hash = $1;`, hasher)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+
+	// 3. Using the UserID from the session, we need to query for that user
+	row = ss.DB.QueryRow(`
+		SELECT email, password_hash FROM users
+		WHERE id = $1;`, user.ID)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+
+	// 4. Return the user
+	return &user, nil
 }
 
 func (ss *SessionService) hash(token string) string {
