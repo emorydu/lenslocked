660ad86960ce9826215f73ac3d57e3a524a6eeef
Emory.Du <Emory163@163.com>
2023-10-07 10:33:51 +0800

    Added gallery feature

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/emorydu/lenslocked/context"
 	"github.com/emorydu/lenslocked/models"
 	"net/http"
 )
@@ -19,4 +21,21 @@ func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
 	}
 	data.Title = r.FormValue("title")
 	g.Templates.New.Execute(w, r, data)
+}
+
+func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		UserID int
+		Title  string
+	}
+	data.UserID = context.User(r.Context()).ID
+	data.Title = r.FormValue("title")
+
+	gallery, err := g.GalleryService.Create(data.Title, data.UserID)
+	if err != nil {
+		g.Templates.New.Execute(w, r, data, err)
+		return
+	}
+	editURL := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editURL, http.StatusFound)
 }
--- main.go
+++ main.go
@@ -103,6 +103,7 @@ func main() {
 		r.Group(func(r chi.Router) {
 			r.Use(umw.RequireUser)
 			r.Get("/new", galleriesC.New)
+			r.Post("/", galleriesC.Create)
 		})
 	})
 
